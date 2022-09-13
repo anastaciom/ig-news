31a5c734be1e7d9876713689c6303c71ed62c35e
Anastacio Menezes <anastaciomenezes@outlook.com>
2022-09-12 23:32:56 -0300

    feat: header navigation

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,18 +1,22 @@
 import Image from "next/image";
 import SignInButton from "./SignInButton/index";
 import styles from "./styles.module.scss";
+import ActiveLink from "./ActiveLink/index";
+
 export default function Header() {
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
         <Image src="/images/logo.svg" alt="Logo" width="110" height="31" />
         <nav>
-          <a className={styles.active} href="">
-            Home
-          </a>
-          <a href="">Posts</a>
+          <ActiveLink activeClassName={styles.active} href="/" prefetch>
+            <a>Home</a>
+          </ActiveLink>
+          <ActiveLink activeClassName={styles.active} href="/posts" prefetch>
+            <a>Posts</a>
+          </ActiveLink>
         </nav>
-        <SignInButton/>
+        <SignInButton />
       </div>
     </header>
   );
